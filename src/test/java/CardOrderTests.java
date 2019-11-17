831ea7ecb38c8527789cc29a8f71035db7ebf7c8
Anna Zabelinskaya <azabela2016@gmail.com>
2019-11-17 15:02:58 +0300

    Added files

---
+++ src/test/java/CardOrderTests.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardOrderTests {
+
+    @Test
+    @DisplayName("Должен успешно отправлять заявку при валидных данных")
+    void shouldSubmitRequest() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input.input__control").setValue("Иванов Иван");
+        $("[data-test-id=phone] input.input__control").setValue("+79991112233");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+
+    @Test
+    @DisplayName("Должен указывать на ошибку при вводе невалидных фамилии и имени")
+    void shouldNotSubmitWithWrongName() {
+        open("http://localhost:9999/");
+        $("[data-test-id=name] input.input__control").setValue("Ivanov Ivan");
+        $("[data-test-id=phone] input.input__control").setValue("+79991112233");
+        $("[data-test-id=agreement]").click();
+        $("button").click();
+        $(".input_invalid").shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+
+
+}
